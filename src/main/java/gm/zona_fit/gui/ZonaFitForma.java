644d056dd3eb4171c598430d43f152904ffe0471
Karina2201 <karinaromero.9822@gmail.com>
2025-08-01 21:49:20 -0600

    Se listan los clientes en la aplicacion

--- src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 @Component
 public class ZonaFitForma extends JFrame{
     private JPanel panelPrincial;
+    private JTable clientesTabla;
     IClienteServicio clienteServicio;
+    private DefaultTableModel tablaDeModeloClientes;
 
     @Autowired
     public ZonaFitForma(ClienteServicio clienteServicio){
@@ -25,4 +28,27 @@ public class ZonaFitForma extends JFrame{
         setLocationRelativeTo(null);
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaDeModeloClientes = new DefaultTableModel(0,4);
+        String[] cabeceros = {"ID", "NOMBRE", "APELLIDO", "MEMBRESIA"};
+        this.tablaDeModeloClientes.setColumnIdentifiers(cabeceros);
+        this.clientesTabla = new JTable(tablaDeModeloClientes);
+        //cargar listado de clientes
+        listarClientes();
+    }
+
+    private void listarClientes() {
+        this.tablaDeModeloClientes.setRowCount(0);
+        var clientes = this.clienteServicio.listarClientes();
+        clientes.forEach(cliente -> {
+            Object[] renglonCliente = {
+                    cliente.getId(),
+                    cliente.getNombre(),
+                    cliente.getApellido(),
+                    cliente.getMembresia()
+            };
+            this.tablaDeModeloClientes.addRow(renglonCliente);
+        });
+    }
 }
