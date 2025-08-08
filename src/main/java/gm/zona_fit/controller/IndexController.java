a855e8a464cd504e810908f9e2197c0f0e8fff3a
Karina2201 <karinaromero.9822@gmail.com>
2025-08-07 21:55:24 -0600

    Se agrega metodo de editar y eliminar

--- src/main/java/gm/zona_fit/controller/IndexController.java
+++ src/main/java/gm/zona_fit/controller/IndexController.java
@@ -3,8 +3,11 @@ package gm.zona_fit.controller;
 import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.servicio.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ public class IndexController {
     @Autowired
     IClienteServicio clienteServicio;
     private List<Cliente> clientes;
+    private Cliente clienteSeleccionado;
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @PostConstruct
@@ -31,6 +35,47 @@ public class IndexController {
     public void cargarDatos(){
         this.clientes = this.clienteServicio.listarClientes();
         this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+    }
+
+    public void agregarCliente(){
+        this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("cliente a guardar: " +this.clienteSeleccionado);
+        //agregar
+        if(this.clienteSeleccionado.getId() == null) {
+            this.clienteServicio.guardarClientePorId(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Agregado"));
+        }
+        //modificar(uppdate)
+        else{
+            this.clienteServicio.guardarClientePorId(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Actualizado"));
+        }
+        //ocultar ventana
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+        //actualizar tabla
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes",
+                "forma-clientes:clientes-tabla");
+        //reset del objeto cliente seleccionado
+        this.clienteSeleccionado = null;
+    }
+
+    public void eliminarCliente(){
+        logger.info("Cliente a eliminar: " + this.clienteSeleccionado);
+        this.clienteServicio.eliminarCliente(this.clienteSeleccionado);
+        //eliminar el registro de la lista de clientes
+        this.clientes.remove(this.clienteSeleccionado);
+        //reset del objeto seleccionado
+        this.clienteSeleccionado = null;
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Cliente eliminado"));
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes",
+                "forma-clientes:clientes-tabla");
     }
 
 }
